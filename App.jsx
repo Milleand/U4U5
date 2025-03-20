e9e7f2513e5508309ff1a37e6a21deaa4b81cfed
Milleand <140829447+Milleand@users.noreply.github.com>
2025-03-20 11:15:04 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,44 @@
+import MovieList from "./MovieList.jsx";
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <MovieList/>
+        </>
+    )
+}
+
+export default App
+
+MovieList.jsx
+
+import { useState } from 'react';
+
+const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+    const [movie, setMovie] = useState('');
+    const [count, setCount] = useState(0)
+
+    function addMovie() {
+        setMovies([...movies, movie])
+        setMovie("")
+        setCount(count + 1 )
+
+    }
+
+    return (
+        <>
+            <text> Count: {count} </text>
+            <input value={movie} onChange={e => setMovie(e.target.value)} />
+            <button onClick={addMovie}>Add Movie</button>
+            <ul>
+                {movies.map((m, index) => <li key={index}>{m}</li>)}
+            </ul>
+        </>
+    )
+
+}
+
+export default MovieList;
